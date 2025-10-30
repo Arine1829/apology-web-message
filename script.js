83c2a539a467b2654102353d98fadaabfdb64d70
Arine1829 <arinehere385@gmail.com>
2025-10-29 21:54:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("playBtn").addEventListener("click", function ()
+{
+    const audio =
+    document.getElementById("bgMusic");
+    audio.play();
+    alert("Thank you for listening to my heart 💗 I LOVE YOU SO FREAKING MUCH MY SAKUU 💗")
+});
+
