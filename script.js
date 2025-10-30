aedd6ebf646da621de5700f6850e6947f23fcfcb
Arine1829 <arinehere385@gmail.com>
2025-10-29 22:01:37 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("playBtn").addEventListener("click", function ()
-{
-    const audio =
-    document.getElementById("bgMusic");
-    audio.play();
-    alert("Thank you for listening to my heart 💗 I LOVE YOU SO FREAKING MUCH MY SAKUU 💗")
-});
-
